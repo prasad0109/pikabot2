d963542b1c5e75bf327be0c9700699e83dbd3c27
prasad0109 <drivezoneyt09@gmail.com>
2026-08-07 14:56:26 +0530

    Add files via upload

---
+++ config/settings.js
@@ -0,0 +1,10 @@
+module.exports = {
+    server: {
+        serverAddress: 'play.pika-network.net', // Minecraft server address
+        // port: 25565, // Minecraft server port
+    },
+    botSettings: {
+        password: 'SANAP@2009',
+        version: '1.18.1', // Minecraft version
+    }
+};
